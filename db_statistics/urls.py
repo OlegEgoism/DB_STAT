ac04756639326be6b0d8eafc417b0ec7301853fb
oleg <olegpustovalov220@gmail.com>
2026-06-24 22:58:11 +0300

    dev v1

--- db_statistics/urls.py
+++ db_statistics/urls.py
@@ -1,10 +1,2 @@
-from django.urls import path
-
-from db_statistics import views
-
-app_name = "db_statistics"
-
 urlpatterns = [
-    path("", views.connection_list, name="connections"),
-    path("connections/<int:pk>/delete/", views.connection_delete, name="connection_delete"),
 ]
