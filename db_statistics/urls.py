c4c385abdf8dc71e2b6c175c8e0fd52450065360
OlegEgoism <81327146+OlegEgoism@users.noreply.github.com>
2026-06-25 00:12:48 +0300

    Refactor home dashboard template

--- db_statistics/urls.py
+++ db_statistics/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+
+from db_statistics import views
+
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("cluster/status/", views.cluster_status, name="cluster_status"),
 ]
