d18acf0f87b8951ca0cd9ca22f1ff8c5278d4c97
OlegEgoism <81327146+OlegEgoism@users.noreply.github.com>
2026-06-26 23:04:52 +0300

    Add schema size table to databases tab

--- db_statistics/urls.py
+++ db_statistics/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("connections/test/", views.test_connection, name="test_connection"),
     path("connections/delete/", views.delete_connection, name="delete_connection"),
     path("databases/size/", views.database_size, name="database_size"),
+    path("databases/schemas/", views.database_schema_sizes, name="database_schema_sizes"),
     path("segments/info/", views.segments_info, name="segments_info"),
 ]
