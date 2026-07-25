5c7b72a96b52603ec3bd3ab6ebd6c462087d1e00
OlegEgoism <81327146+OlegEgoism@users.noreply.github.com>
2026-07-26 01:10:25 +0300

    Restyle project 404 page

--- db_statistics/tests.py
+++ db_statistics/tests.py
@@ -7,8 +7,8 @@ class PageNotFoundTests(TestCase):
 
         self.assertEqual(response.status_code, 404)
         self.assertTemplateUsed(response, "404.html")
-        self.assertContains(response, "Transaction Rollback", status_code=404)
-        self.assertContains(response, "'/missing-page/'", status_code=404)
+        self.assertContains(response, "Страница не найдена | DB STAT", status_code=404)
+        self.assertContains(response, "/missing-page/", status_code=404)
         self.assertContains(response, "ABORTED", status_code=404)
         self.assertContains(response, 'href="/"', status_code=404)
 
